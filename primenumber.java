9ac5b874a8b41c416233e8b32e6fce0dc5964433
Malathi Ramesh <malathiramesh0310@gmail.com>
2026-07-28 22:29:29 +0530

    Added primenumber program

---
+++ primenumber.java
@@ -0,0 +1,19 @@
+public class primenumber {
+    public static void main(String[] args) {
+
+        int num = 17;
+        boolean prime = true;
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                prime = false;
+                break;
+            }
+        }
+
+        if (prime)
+            System.out.println("Prime Number");
+        else
+            System.out.println("Not Prime");
+    }
+}
