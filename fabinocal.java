72b45db7ad811a81166174938e75f545a520da97
Malathi Ramesh <malathiramesh0310@gmail.com>
2026-07-28 22:28:21 +0530

    Added fabinocal program

---
+++ fabinocal.java
@@ -0,0 +1,16 @@
+public class fabinocal {
+    public static void main(String[] args) {
+
+        int n = 10;
+        int a = 0, b = 1;
+
+        System.out.print(a + " " + b + " ");
+
+        for (int i = 3; i <= n; i++) {
+            int c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            b = c;
+        }
+    }
+}
